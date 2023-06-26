efdcc444784f30ae55ef277d7e3a367646e340a5
WonWonmmi <jlx0626sammi@163.com>
2023-06-26 22:13:26 +0800

    commit add function

--- shudu.cpp
+++ shudu.cpp
@@ -7,7 +7,7 @@
 #define BOARD_SIZE 9
 #define EMPTY 0
 
-// Function to check if a number can be placed at a given position in the board
+// Function to check if a number can be placed at a given position([row][col]) in the board
 bool isSafe(const std::vector<std::vector<int>>& board, int row, int col, int num) {
     // Check if the number already exists in the same row or column
     for (int i = 0; i < BOARD_SIZE; i++) {
@@ -22,6 +22,33 @@ bool isSafe(const std::vector<std::vector<int>>& board, int row, int col, int nu
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (board[i + gridStartRow][j + gridStartCol] == num) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+// Function to solve the Sudoku puzzle using backtracking
+bool solveSudoku(std::vector<std::vector<int>>& board) {
+    for (int row = 0; row < BOARD_SIZE; row++) {
+        for (int col = 0; col < BOARD_SIZE; col++) {
+            // Find an empty cell
+            if (board[row][col] == EMPTY) {
+                // Try placing numbers from 1 to 9
+                for (int num = 1; num <= 9; num++) {
+                    if (isSafe(board, row, col, num)) {
+                        board[row][col] = num;
+
+                        if (solveSudoku(board)) {
+                            return true;
+                        }
+
+                        board[row][col] = EMPTY;
+                    }
+                }
+
                 return false;
             }
         }
